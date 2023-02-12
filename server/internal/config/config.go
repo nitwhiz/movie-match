735c33af4e347ee2aae6e85fc683328e9cb9a8dc
nitwhiz <schonfeldandy@gmail.com>
2023-02-12 14:07:14 +0100

    add Dockerfile for app and server

--- server/internal/config/config.go
+++ server/internal/config/config.go
@@ -3,6 +3,8 @@ package config
 import "github.com/spf13/viper"
 
 func Init() error {
+	viper.SetEnvPrefix("MM")
+
 	viper.SetConfigName("config")
 	viper.SetConfigType("yaml")
 	viper.AddConfigPath(".")
